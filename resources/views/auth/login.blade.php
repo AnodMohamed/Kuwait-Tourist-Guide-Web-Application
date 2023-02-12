0fe62f8d03b654572b32eaa170ed79fc3d141bd2
AnodMohamed <96368212+AnodMohamed@users.noreply.github.com>
2023-02-12 05:06:07 -0800

    Log in and register

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,48 @@
+@section('title')
+تسجبل الدخول 
+@stop  
+
+<x-guest-layout>
+    <x-jet-authentication-card>
+        <x-slot name="logo">
+            <x-jet-authentication-card-logo />
+        </x-slot>
+
+        <x-jet-validation-errors class="mb-4" />
+
+        @if (session('status'))
+            <div class="mb-4 font-medium text-sm text-green-600">
+                {{ session('status') }}
+            </div>
+        @endif
+
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+
+            <div>
+                <x-jet-label for="email" value="{{ __('البريد الإلكتروني ') }}" />
+                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            </div>
+
+            <div class="mt-4">
+                <x-jet-label for="password" value="{{ __('كلمة المرور') }}" />
+                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+            </div>
+
+            <div class="block mt-4">
+                <label for="remember_me" class="flex items-center">
+                    <x-jet-checkbox id="remember_me" name="remember" />
+                    <span class="ml-2 text-sm text-gray-600">{{ __('ذكرني ') }}</span>
+                </label>
+            </div>
+
+            <div class="flex items-center justify-start mt-4">
+                
+
+                <x-jet-button class="mr-4">
+                    {{ __('تسجيل الدخول') }}
+                </x-jet-button>
+            </div>
+        </form>
+    </x-jet-authentication-card>
+</x-guest-layout>
