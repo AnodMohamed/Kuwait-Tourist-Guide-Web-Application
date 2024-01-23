415a52b8b9c3e94f1e231ebf2f25846894fbbdac
AnodMohamed <anodmohamed2020@gmail.com>
2024-01-23 12:05:16 +0300

    last

---
+++ Kuwait-Tourist-Guide-Web-Application-main/resources/views/layouts/footer.blade.php
@@ -0,0 +1,15 @@
+	<!-- Copyright -->
+    <div class="copyright">&copy; حقوق النشر : دليل الكويت السياحي.</div>
+
+</div>
+
+</div>
+
+<!-- Scripts -->
+<script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+<script src="{{ asset('assets/js/browser.min.js') }}"></script>
+<script src="{{ asset('assets/js/breakpoints.min.js') }}"></script>
+<script src="{{ asset('assets/js/main.js') }}"></script>
+
+</body>
+</html>
---
+++ Kuwait-Tourist-Guide-Web-Application-main/resources/views/layouts/header.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE HTML>
+<!--
+	Ethereal by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html lang="ar" dir="rtl">
+	<head>
+        <title>@yield("title")</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+		<link rel="stylesheet" href="{{ asset('assets/css/main.css') }}" />
+		<noscript><link rel="stylesheet" href="{{ asset('assets/css/noscript.css') }}" /></noscript>
+
+	</head>
+	<body class="is-preload">
+		
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
+			
+				<!-- Wrapper -->
+					<div id="wrapper">
+						<nav class="navbar navbar-expand-lg navbar-light  d-block">
+							<a class="navbar-brand" href="#">Navbar</a>
+							<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+							  <span class="navbar-toggler-icon"></span>
+							</button>
+							<div class="collapse navbar-collapse "  id="navbarNav">
+							  <ul class="navbar-nav d-block">
+								<li class="nav-item active">
+								  <a class="nav-link" href="#">الصفحة الرئيسية <span class="sr-only">(current)</span></a>
+								</li>
+								<li class="nav-item">
+								  <a class="nav-link" href="{{ route('login') }}">تسجبل الدخول  </a>
+								</li>
+								<li class="nav-item">
+								  <a class="nav-link" href="{{ route('register') }}">إنشاء الحساب
+                                </a>
+								</li>
+								<li class="nav-item">
+								  <a class="nav-link disabled" href="#">Disabled</a>
+								</li>
+							  </ul>
+							</div>
+						</nav>
